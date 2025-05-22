3fec5cc31baa6728bbae8fdfbba8b906f94a9a3c
fbwoduf112 <fbwoduf2@gmail.com>
2025-05-22 11:17:21 +0900

    주문 수정

--- src/main/java/com/example/demo/KakaoPay/KakaoPayController.java
+++ src/main/java/com/example/demo/KakaoPay/KakaoPayController.java
@@ -1,7 +1,13 @@
 package com.example.demo.KakaoPay;
 
+import com.example.demo.Service.CustomerOrderService;
+import com.example.demo.dto.OrderDTO;
+import com.example.demo.entity.customer.Customer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -9,9 +15,17 @@ import org.springframework.web.bind.annotation.*;
 public class KakaoPayController {
 
     private final KakaoPayProvider kakaoPayProvider;
+    private final CustomerOrderService customerOrderService;
+
+//    @PostMapping("/ready")
+//    public KakaoPayResponse.ReadyResponse ready(@RequestBody KakaoPayRequest.OrderRequest request){
+//        return kakaoPayProvider.ready(request);
+//    }
 
     @PostMapping("/ready")
-    public KakaoPayResponse.ReadyResponse ready(@RequestBody KakaoPayRequest.OrderRequest request){
+    public KakaoPayResponse.ReadyResponse ready(@RequestBody List<OrderDTO> orders, @AuthenticationPrincipal Customer customer, @RequestParam Long storeId){
+
+        KakaoPayRequest.OrderRequest request = customerOrderService.order(orders, customer, storeId);;
         return kakaoPayProvider.ready(request);
     }
 
