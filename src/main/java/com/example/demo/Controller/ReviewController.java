62b95967dbffcf944f04162c4a817aed11c3e99e
fbwoduf112 <fbwoduf2@gmail.com>
2025-05-29 10:24:10 +0900

    리뷰 목록 보여주기

---
+++ src/main/java/com/example/demo/Controller/ReviewController.java
@@ -0,0 +1,36 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Service.ReviewService;
+import com.example.demo.dto.UnreviewedStatisticsDto;
+import com.example.demo.entity.customer.Customer;
+import com.example.demo.entity.entityInterface.AppUser;
+import com.example.demo.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+    private final CustomerRepository customerRepository;
+    private final ReviewService reviewService;
+    @GetMapping("/review/ListShow")
+    public ResponseEntity<?> reviewList(@AuthenticationPrincipal AppUser user) {
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        Customer customer = customerRepository.findById(user.getId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        List<UnreviewedStatisticsDto> reviewList = reviewService.getUnreviewedStatisticsByCustomer(customer);
+        return ResponseEntity.ok(reviewList);
+
+    }
+
+}
