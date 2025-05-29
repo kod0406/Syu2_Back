6d4a34ddd0e0f0b926aee518752c3800759f94ac
fbwoduf112 <fbwoduf2@gmail.com>
2025-05-29 16:22:17 +0900

    리뷰 작성

--- src/main/java/com/example/demo/dto/ReviewWriteDTO.java
+++ src/main/java/com/example/demo/dto/ReviewWriteDTO.java
@@ -1,5 +1,14 @@
 package com.example.demo.dto;
 
+import com.example.demo.Service.Amazon.S3UploadService;
+import com.example.demo.entity.common.CustomerReviewCollect;
+import com.example.demo.entity.common.CustomerStatistics;
+import com.example.demo.entity.customer.Customer;
+import com.example.demo.entity.store.Store;
+import com.example.demo.entity.store.StoreMenu;
+import com.example.demo.repository.CustomerRepository;
+import com.example.demo.repository.CustomerStatisticsRepository;
+import com.example.demo.repository.StoreRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,13 +22,21 @@ import java.util.List;
 @Slf4j
 public class ReviewWriteDTO {
     private Long statisticsId;
-    private String storeName;
-    private String orderDetails;
-    private long orderPrice;
-    private long orderAmount;
     private LocalDate date;
     private String comment;
     private Double reviewRating;
-    private List<MultipartFile> images;
-    private List<String> imageUrls;
+    private MultipartFile images;
+    private String imagesUrl;
+    public CustomerReviewCollect toEntity(Customer customer, Store store, CustomerStatistics customerStatistics, StoreMenu storeMenu) {
+        CustomerReviewCollect customerReviewCollect = CustomerReviewCollect.builder()
+                        .score(this.reviewRating)
+                        .reviewDetails(this.comment)
+                        .reviewDate(this.date)
+                        .imageUrl(this.imagesUrl)
+                        .customer(customer)
+                        .storeMenu(storeMenu)
+                        .store(store)
+                        .build();
+        return customerReviewCollect;
+    }
 }
