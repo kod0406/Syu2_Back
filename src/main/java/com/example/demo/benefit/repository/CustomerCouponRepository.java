e52c2acf68e95585553fa0e3e8bb575de646a617
kod0406 <kod04061@gmail.com>
2025-06-25 15:09:58 +0900

    주문 현황에서 쿠폰과 포인트 사용 내역이 제대로 표시 되도록 수정

--- src/main/java/com/example/demo/benefit/repository/CustomerCouponRepository.java
+++ src/main/java/com/example/demo/benefit/repository/CustomerCouponRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.benefit.repository;
 
+import com.example.demo.benefit.entity.Coupon;
 import com.example.demo.customer.entity.CustomerCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,4 +14,12 @@ public interface CustomerCouponRepository extends JpaRepository<CustomerCoupon, 
     Optional<CustomerCoupon> findByCustomerIdAndCouponId(Long customerId, Long couponId);
     List<CustomerCoupon> findByCustomerId(Long customerId);
     void deleteByExpiresAtBefore(LocalDateTime now);
+
+
+    @Query("""
+        SELECT c.coupon.couponName
+        FROM CustomerCoupon c
+        WHERE c.couponUuid = :uuid
+        """)
+    String findCouponNameByCouponUuid(@Param("uuid") String uuid);
 }
