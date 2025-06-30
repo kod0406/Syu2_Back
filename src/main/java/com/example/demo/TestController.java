91e9f0344e5eb59a05546805c126c5e21cc53ddc
fbwoduf112 <fbwoduf2@gmail.com>
2025-06-30 11:54:33 +0900

    CICD

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
     @GetMapping("/test/test/CICD")
     private String testCICD() {
-        return "testCICD";
+        return "testCICD is Working~!";
     }
 }
